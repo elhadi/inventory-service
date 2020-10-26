ad97d9f57c15b4eeb398bd7c22580a597b1afdb9
El Hadi AIT MAHIDDINE <eh.ait.mahiddine@apefactory.com>
2020-10-26 11:17:09 +0100

    unit test

--- src/main/java/atm/cortex/designer/model/Status.java
+++
@@ -1,8 +0,0 @@
-package atm.cortex.designer.model;
-
-public enum Status {
-
-    DRAFT, VALIDATED, ACTIVE, DESABLED, EXPIRED
-
-
-}
--- src/main/java/com/shoppyng/cart/Application.java
+++ src/main/java/com/shoppyng/cart/Application.java
@@ -1,4 +1,4 @@
-package atm.cortex.designer;
+package com.shoppyng.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
